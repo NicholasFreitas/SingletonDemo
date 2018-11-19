964ea3417913ad36427faf5af1f1a7da8f89a5bd
Nicholas Freitas <nicholasfreitas@sympatico.ca>
2018-11-19 16:19:52 -0500

    Small application that demonstrates singleton
    Demo of singleton pattern

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    //Intent: ensure a class has one instance and provide a global point of access to it.
+    /*
+     -----------------------
+     |Singleton            |
+     -----------------------
+     |static Instance()    |
+     |SingletonOperation() |    
+     |GetSingletonData()   |    
+     -----------------------
+     |static uniqueInstance|
+     |singletonData        |
+     -----------------------
+         
+         */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var singletonObject = SingletonDemo.GetInstance();
+
+            singletonObject.DoSomethingWiththeSingleton();
+
+        }
+    }
+}
---
+++ SingletonDemo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    class SingletonDemo
+    {
+        private static SingletonDemo currentInstance;
+
+        private SingletonDemo()
+        {
+        }
+
+        public static SingletonDemo GetInstance()
+        {
+            if (currentInstance == null)
+                currentInstance = new SingletonDemo();
+            
+            return currentInstance;
+        }
+
+        public void DoSomethingWiththeSingleton()
+        {
+            Console.WriteLine("Singleton doing something.");
+        }
+
+
+
+    }
+}
